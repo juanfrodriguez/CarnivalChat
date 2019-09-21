ca1c67e75fbf854d1f22783b348e69e33ebf20b1
Juan Felipe Rodriguez <juan.feliperodriguez@yahoo.com>
2019-09-21 03:31:17 -0400

    Initial Commit

---
+++ CarnivalChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarnivalChat
+//
+//  Created by Juan Felipe Rodriguez Van on 9/21/19.
+//  Copyright © 2019 CarnivalChatGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
